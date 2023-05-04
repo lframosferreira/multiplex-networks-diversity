491d06fee881772860bcbca56a7d0b5dab9a044b
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-05-03 23:19:06 -0300

    [NEW] renaming some variables and trying to refactor the diversity code

--- diversity_measure.py
+++ diversity_measure.py
@@ -1,7 +1,6 @@
 import itertools
 import numpy as np
 import numpy.typing as npt
-
 from scipy.spatial.distance import jensenshannon as JSD
 from scipy.sparse.csgraph import shortest_path
 
@@ -10,7 +9,7 @@ from scipy.sparse.csgraph import shortest_path
 @type graph: npt.NDArray[np.int_]
 @param graph: the graph whose node distribution will be calculated
 
-@rtype: npt.NDArray
+@rtype: npt.NDArray[np.float_]
 @returns: the node distribution of the graph
 """
 def node_distance_distribution(graph: npt.NDArray[np.int_]) -> npt.NDArray[np.float_]:
@@ -25,26 +24,26 @@ def node_distance_distribution(graph: npt.NDArray[np.int_]) -> npt.NDArray[np.fl
 @type graph: npt.NDArray[np.int_]
 @param graph: the graph whose transisiton matrix will be calculated
 
-@rtype: npt.NDArray
+@rtype: npt.NDArray[np.float_]
 @returns: the transition matrix of the graph in the same shape of the graph
 """
-def transition_matrix(graph: npt.NDArray[np.int_]) -> npt.NDArray:   
+def transition_matrix(graph: npt.NDArray[np.int_]) -> npt.NDArray[np.float_]:   
     transition_matrix: npt.NDArray[np.float_] = graph/np.sum(graph, axis=0)[:, None]
     transition_matrix[np.isnan(transition_matrix)] = 0
     return transition_matrix
 
 """Calculates the layer difference between two layers/graphs
 
-@type node_dist_G: npt.NDArray[np.int_]
+@type node_dist_G: npt.NDArray[np.float_]
 @param node_dist_G: the graph whose transisiton matrix will be calculated
 
-@type trans_mat_G: npt.NDArray[np.int_]
+@type trans_mat_G: npt.NDArray[np.float_]
 @param trans_mat_G: the graph whose transisiton matrix will be calculated
 
-@type node_dist_H: npt.NDArray[np.int_]
+@type node_dist_H: npt.NDArray[np.float_]
 @param node_dist_H: the graph whose transisiton matrix will be calculated
 
-@type trans_mat_H: npt.NDArray[np.int_]
+@type trans_mat_H: npt.NDArray[np.float_]
 @param trans_mat_H: the graph whose transisiton matrix will be calculated
 
 @rtype: np.float_
@@ -64,23 +63,22 @@ def layer_difference(node_dist_G, trans_mat_G, node_dist_H, trans_mat_H) -> np.f
     node_difference: npt.NDArray[np.float_] = (node_distance_distribution_diff + transition_matrix_diff) / 2
     return np.around(np.average(node_difference), decimals=4)
 
-
 """Calculates the less contribute list of the layer/graph network
 
-@type node_dist_G: npt.NDArray
+@type node_dist_G: npt.NDArray[np.float_]
 @param node_dist_G: the list of node distributions for each layer/graph
 
-@type node_dist_G: npt.NDArray
+@type node_dist_G: npt.NDArray[np.float_]
 @param node_dist_G: the list of transition matrices for each layer/graph
 
 @rtype: npt.NDArray[np.int_]
 @returns: the less contribute list
 """
-def less_contribute(node_distance_distributions: npt.NDArray[np.float_], trasition_matrices: npt.NDArray[np.float_]) -> npt.NDArray[np.int_]:
+def less_contribute_rank(node_distance_distributions: npt.NDArray[np.float_], trasition_matrices: npt.NDArray[np.float_]) -> npt.NDArray[np.int_]:
 
-    number_of_layers: np.int_ = len(trasition_matrices)
+    number_of_layers: np.int_ = trasition_matrices.shape[0]
     combinations: list[tuple[int]] = list(itertools.combinations(np.arange(number_of_layers), 2))
-    layer_difference_matrix = np.zeros(shape=((number_of_layers, number_of_layers)), dtype=float)
+    layer_difference_matrix: npt.NDArray[np.float_] = np.zeros(shape=((number_of_layers, number_of_layers)), dtype=np.float_)
     
     for i, j in combinations:
         ld: np.float_ = layer_difference(
@@ -91,31 +89,28 @@ def less_contribute(node_distance_distributions: npt.NDArray[np.float_], trasiti
         )
         layer_difference_matrix[i][j] = layer_difference_matrix[j][i] = ld  
 
-    div: np.float_ = 0
+    diversity: np.float_ = 0.0
 
     np.fill_diagonal(layer_difference_matrix, 1)
 
-    list_less_contribute: list = list()
-    candidatos = list(range(0, layer_difference_matrix.shape[0]))
+    rank: list = list()
 
-    for _ in range(layer_difference_matrix.shape[0]-1):    
+    for _ in range(layer_difference_matrix.shape[0] - 1):    
      
         layer_a, layer_b = np.unravel_index(layer_difference_matrix.argmin(), layer_difference_matrix.shape)
 
         smallest_layer_difference: np.float_ = layer_difference_matrix[layer_a, layer_b]
 
-        div += smallest_layer_difference
+        diversity += smallest_layer_difference
 
-        dist_a_to_set = np.amin(layer_difference_matrix[layer_a], where = layer_difference_matrix[layer_a] != smallest_layer_difference, initial = np.inf)
-        dist_b_to_set = np.amin(layer_difference_matrix[layer_b], where = layer_difference_matrix[layer_b] != smallest_layer_difference, initial = np.inf)
+        dist_a_to_set = np.amin(layer_difference_matrix[layer_a], where=layer_difference_matrix[layer_a] != smallest_layer_difference, initial=np.inf)
+        dist_b_to_set = np.amin(layer_difference_matrix[layer_b], where=layer_difference_matrix[layer_b] != smallest_layer_difference, initial=np.inf)
 
-        less_contribute = layer_a if dist_a_to_set <= dist_b_to_set else layer_b
+        less_contribute_layer: np.int_ = layer_a if dist_a_to_set <= dist_b_to_set else layer_b
         
-        list_less_contribute.append(less_contribute)
+        rank.append(less_contribute_layer)
 
-        layer_difference_matrix[less_contribute,:] = np.inf
-        layer_difference_matrix[:,less_contribute] = np.inf
-    
-    list_less_contribute.append(np.where(np.isin(candidatos, list_less_contribute) == False)[0][0])
+        layer_difference_matrix[less_contribute_layer,:] = np.inf
+        layer_difference_matrix[:,less_contribute_layer] = np.inf
 
-    return np.array(list_less_contribute)
+    return np.array(rank)
